0bbc31cf0f35c6b897abbad782bab81afb1790e8
Ashley Trowell <ashley.trowell@matrixresearch.com>
2025-02-06 16:52:36 -0500

    Testing function_traits. Works for  Return type, arity, and argument<N>

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <type_traits>
 
 #include "ruak/param_pack.hpp"
 #include "ruak/utilities.hpp"
 
 // Test function for function_traits
 double foo(int, float) { return 0.0; }
+void bar(int, float) {}
 
 int main()
 {
@@ -18,4 +20,9 @@ int main()
    using foo_traits = ruak::function_traits<decltype(&foo)>;
    static_assert(foo_traits::arity == 2, "foo should have 2 parameters");
    std::cout << "Foo arity is : " << foo_traits::arity << "\n";
+
+   using bar_traits = ruak::function_traits<decltype(&bar)>;
+   static_assert(std::is_same_v<bar_traits::return_type, void>, "bar should return void but failed to.\n");
+
+   static_assert(std::is_same_v<int, foo_traits::argument<0>>, "Foo arg 0 should be int");
 }
